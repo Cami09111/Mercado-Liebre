7fed72eac55d56a1bd4a84318120f0c1d3313313
Cami09111 <121905232+Cami09111@users.noreply.github.com>
2023-01-30 15:37:56 -0500

    mercado liebre

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
+
+
+const port = 3000;
+app.listen(port, () =>{
+console.log(`server started on http://localhost:${port}`);
+});
