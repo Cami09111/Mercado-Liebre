d3318abf2b837eed619b7d44f182f7261a9c1ce3
Cami09111 <121905232+Cami09111@users.noreply.github.com>
2023-02-07 21:57:31 -0500

    url

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/login', (req, res) =>{
 });
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () =>{
 console.log(`server started on http://localhost:${port}`);
 });
